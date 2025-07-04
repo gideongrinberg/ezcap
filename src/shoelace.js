4ea9fdee68c0be0aa608b092cc73b4de8f5129f4
Gideon Grinberg <grinberg1165@gmail.com>
2025-07-04 13:57:24 -0400

    Remove google fonts, add sl-icon import

--- src/shoelace.js
+++ src/shoelace.js
@@ -4,3 +4,4 @@ import "@shoelace-style/shoelace/dist/components/button/button.js";
 import "@shoelace-style/shoelace/dist/components/button-group/button-group.js";
 import "@shoelace-style/shoelace/dist/components/spinner/spinner.js";
 import "@shoelace-style/shoelace/dist/components/alert/alert.js";
+import '@shoelace-style/shoelace/dist/components/icon/icon.js';
